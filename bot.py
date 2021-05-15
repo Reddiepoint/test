148db30cce17c6af32b190794a6394dc184e988c
Matthew Lim <matthewlim230505@gmail.com>
2021-05-15 18:28:00 +0800

    a

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 from discord.ext.commands import MissingPermissions, MissingRequiredArgument, MissingAnyRole, CommandInvokeError, MemberNotFound
 import asyncio
-import psycopg2
 import youtube_dl
 
 
